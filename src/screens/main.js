36aabe75c390ba382c012f8164ee97522316c09e
funnyjerry <wenhu95piao@gmail.com>
2019-05-08 15:17:23 +0800

    add color

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import Carousel from 'react-native-banner-carousel';
-import {
-  StyleSheet,
-  Image,
-  View,
-  Dimensions,
-  Text,
-  WebView,
-} from 'react-native';
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile-rn'
-import { BANNERS,THEME } from '../config';
+import { StyleSheet, Image, View, Dimensions, Text } from 'react-native';
 import * as shape from 'd3-shape'
 import { AreaChart, Grid } from 'react-native-svg-charts'
+import MaterialTabs from 'react-native-material-tabs';
+import colors from '../utils/colors';
 
-const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
 
 export default class App extends React.Component {
-  renderPage(image, index) {
-    return (
-      <View key={index}>
-        <Image
-          style={{ width: BANNERS.WIDTH-20, height: BANNERS.HEIGHT }}
-          source={{ uri: image }}
-        />
-      </View>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 0,
+    };
   }
+
+  setTab = selectedTab => {
+    this.setState({ selectedTab });
+  };
 
   render() {
     return (
       <View style={styles.container} >
-       
-
-        <WhiteSpace />
 
         <AreaChart
-                style={{ height: 200 }}
-                data={ data }
-                contentInset={{ top: 30, bottom: 30 }}
-                curve={ shape.curveNatural }
-                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
-            >
-                <Grid/>
-            </AreaChart>
+          style={{ height: 200 }}
+          data={data}
+          contentInset={{ top: 30, bottom: 30 }}
+          curve={shape.curveNatural}
+          svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+        >
+          <Grid />
+        </AreaChart>
 
-        <WhiteSpace />
+        <MaterialTabs
+          items={["other:sort_by", "other:name", "other:hours", "other:wages"]}
+          selectedIndex={this.state.selectedTab}
+          onChange={this.setTab}
+          barColor={colors.WHITE}
+          indicatorColor={colors.GREEN}
+          activeTextColor={colors.DARK}
+          inactiveTextColor={colors.GREY2}
+          textStyle={{ fontSize: 12 }}
+        />
 
-      
       </View>
     );
   }
@@ -55,7 +53,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: THEME.BACKGROUNDCOLOR,
-    padding:10
+    backgroundColor: '#fff',
+    padding: 10
   },
 });
